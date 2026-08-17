4731c0d7cbcf0bf80e23e47d45177540232819d7
kamalrajms <kamalrajindtn@gmail.com>
2026-08-17 13:51:55 +0530

    react intro

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Greeting from "./Greeting";
+
+export default function App() {
+  return (
+    <>
+      <h1>hello world</h1>
+      <Greeting/>
+    </>
+    
+  );
+}
---
+++ src/Greeting.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hello from "./Hello";
+
+export default function Greeting() {
+  return (
+    <div>
+      <h1>react intro</h1>
+      <Hello />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        hic corrupti ex voluptate beatae obcaecati excepturi vel ratione
+        officia? Voluptate.
+      </p>
+    </div>
+  );
+}
